fb70635eab8492341ea752716e6d7a1c607fa57f
IPSIT SINGH <ipsitsingh61@gmail.com>
2023-03-11 17:18:49 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+
+def main_screen():
+    screen=Tk()
+    screen.geometry("375x398")
+    
+    #icon
+    image_icon=PhotoImage(file="keys.png")
+    screen.iconphoto(False,image_icon)
+    screen.title("PctApp")
+    Label(text="Enter text for encryption and decryption",fg="black",font=("calbri",13)).place(x=10,y=10)
+
+    screen.mainloop() 
+
+main_screen()
